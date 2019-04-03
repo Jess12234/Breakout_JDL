873c8932737b7f7fd145fbd7caf49b96af538d04
Jessica Lowry <jdl14a@dmc-207-04.local>
2019-04-03 08:22:54 -0500

    it do the bounce

--- Bamboo Breakout/GameScene.swift
+++ Bamboo Breakout/GameScene.swift
@@ -35,7 +35,9 @@ class GameScene: SKScene {
   
   override func didMove(to view: SKView) {
     super.didMove(to: view)
-    
+    let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+    borderBody.friction = 0
+    self.physicsBody = borderBody
   }
   
   
